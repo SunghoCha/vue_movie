2191e4babd292f0c109bc118f6e1dc63a0d5b3d5
SunghoCha <tjdgh3725@gmail.com>
2024-07-02 15:59:11 +0900

    vue-movie event,modal,navbar 컴포넌트 구현

---
+++ movie-info/src/components/Modal.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="modal" v-if="isModal">
+    <div class="inner">
+      <h3>{{ data[selectedMovie].title }}</h3>
+      <p>영화 상세정보</p>
+      <div></div>
+      <button @click="$emit('closeModal')">닫기</button>  <!-- 부모에게 $emit('이벤트명')으로 변경요청-->
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    name: "ModalComponent",
+    props: {
+        data: Array,
+        isModal: Boolean, // props로 전달받은 데이터는 부모만 변경 가능(닫기 버튼 클릭시 부모의 isModal 데이터 직접 변경 불가) 자식이 부모에게 데이터변경 요청가능
+        selectedMovie: Number, 
+    }
+}
+</script>
+<style>
+    
+</style>
---
+++ movie-info/src/components/SearchBar.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="search-box">
+        <input type="search" 
+        @change="inputText = $event.target.value"
+        placeholder="검색어 입력"
+        >
+        <button>검색</button>
+    </div>
+    <p>{{ inputText }}</p>
+</template>
+<script>
+export default {
+    name:"SearchBarComponent",
+    data() {
+        return {
+            inputText: '',
+        }
+    
+    },
+    props: {
+        data:Array,
+    },
+    watch: { // 검사할 변수명(변경값) {로직}
+        inputText(name) {
+            // 입력한 영화제목이 데이터에 있는지 확인
+            const findName = this.data.filter(item => {
+                return item.title.includes(name);
+            })
+            console.log(findName);
+        }
+    }
+}
+</script>
+<style>
+    
+</style>

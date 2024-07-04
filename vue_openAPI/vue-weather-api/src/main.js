220f2b8bf33882c9486604c2720d8bfbd730be42
SunghoCha <tjdgh3725@gmail.com>
2024-07-04 17:44:54 +0900

    weather_API - 검색창 추가

--- vue_openAPI/vue-weather-api/src/main.js
+++ vue_openAPI/vue-weather-api/src/main.js
@@ -11,9 +11,10 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 // 사용할 아이콘 불러오기
 import { faBarsStaggered } from '@fortawesome/free-solid-svg-icons'
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 // 아이콘 등록하기
-library.add(faBarsStaggered, faLocationDot);
+library.add(faBarsStaggered, faLocationDot, faMagnifyingGlass);
 
 createApp(App)
     .component('font-awesome-icon', FontAwesomeIcon)

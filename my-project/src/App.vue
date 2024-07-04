82f1e19073672dd04ba173487fcd959df3334427
SunghoCha <tjdgh3725@gmail.com>
2024-07-04 10:19:12 +0900

    template 타이틀 영억 디자인

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,34 +1,84 @@
 <script setup>
-  import { ref, onMounted, watch } from 'vue';
-  import ChildComp from './components/ChildComp.vue';
 
-  let count = ref(0); // 상태변수 초기값(state)
-  let title = "Hello Vue"
-  let color = ref('green');
-
-  onMounted(() => {
-    console.log('mounted');
-  })
-
-  const increaseCount = () => {
-    count.value++; // ref 안에 있는 값을 가져오는 컨셉
-  }
-
-  watch(color, () => {
-    console.log('color 변경');
-  })
 </script>
 
 <template>
-  <div>
-    <h1>{{ title }}</h1>
-      <button @click="increaseCount">count++</button>
-      <p>{{ count }}</p>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">GitHub</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
   </div>
-  <ChildComp :color=color bgColor="yellow"/>
-  <button @click="color='red'">change color</button>
+</nav>
+  <section id="title">
+    <div class="contatiner">
+      <div class="detail row">
+        <figure class="col-md-6">
+          <img src="./assets/images/title.png" alt="">
+        </figure>
+        <div class="text col-md-6">
+          <h2>Hello,<br>I'm ?</h2>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+#title {
 
-</style>
+  .container {
+    $navbar_height: 56px; // navbar 높이값
+    height: 100vh;
+    padding: $navbar_height;
+
+    .row {
+      height: 100%;
+      flex-direction: row-reverse;
+    }
+  }
+
+  figure {
+    img {
+      width: 100%;
+    }
+    display: grid;
+    place-items: center;
+  }
+
+  .text {
+    h2 {
+      font-size: 4em;
+    }
+
+    display: grid;
+    place-items: center;
+  }
+}</style>

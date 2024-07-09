4db22f6c093fec13e8792d8b42520cc86efa85f6
SunghoCha <tjdgh3725@gmail.com>
2024-07-09 20:50:33 +0900

    이벤트 바인딩 이해하기

---
+++ vue_standard procedure/basics-03-events-starting-code/app.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    addCount() {
+      this.counter++;
+    },
+    removeCount() {
+      this.counter--;
+    }
+  },
+});
+
+app.mount('#events');

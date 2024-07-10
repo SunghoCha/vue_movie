fa7e7f29675377721d78b504e6be7c6c06cdf83c
SunghoCha <tjdgh3725@gmail.com>
2024-07-10 22:41:52 +0900

    컨텐츠 조건부 렌더링 및 목록 렌더링

--- vue_standard procedure/lists-cond-01-starting-setup/app.js
+++ vue_standard procedure/lists-cond-01-starting-setup/app.js
@@ -1,7 +1,28 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return { 
+      goals: [], 
+      goalText : '',
+    };
   },
+  computed: {
+    printGoal() {
+      if (this.goals.length === 0) {
+        return 'No goals have been added yet - please start adding some!';
+      } else {
+        return '';
+      }
+    }
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.goalText);
+      this.goalText='';
+    },
+    removeGoal(index) {
+      this.goals.splice(index, 1);
+    }
+  }
 });
 
 app.mount('#user-goals');

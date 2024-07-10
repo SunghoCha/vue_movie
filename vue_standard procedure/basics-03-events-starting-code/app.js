9408662f3f584dbd202059c75ff53a17f68f01ba
SunghoCha <tjdgh3725@gmail.com>
2024-07-10 10:15:06 +0900

    이벤트 바인딩 실습(인수, 네이티브 이벤트 객체 활용 등)

--- vue_standard procedure/basics-03-events-starting-code/app.js
+++ vue_standard procedure/basics-03-events-starting-code/app.js
@@ -2,14 +2,26 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
+      confirmedName: '',
+     
     };
   },
   methods: {
+    confirmedInput() {
+      this.confirmedName = this.name;
+    },
+    submitForm() {
+      alert('Submitted!')
+    },
     addCount() {
       this.counter++;
     },
     removeCount() {
       this.counter--;
+    },
+    setName(event, text) {
+      this.name = event.target.value + ' ' + text;
     }
   },
 });

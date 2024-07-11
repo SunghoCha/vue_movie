804ba6dec179fb1237ed833c001440db9b6e0245
SunghoCha <tjdgh3725@gmail.com>
2024-07-11 21:22:52 +0900

    Component - 부분 커밋

---
+++ vue_standard procedure/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <h2>My Friends</h2>
+    <ul>
+        <li v-for="friend in friends" :key="friend.id">
+            {{ friend }}
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                friends: [
+                    {
+                        id: 'manuel',
+                        name: 'Manuel Lorenz',
+                        phone: '0123 45678 90',
+                        email: 'manuel@localhost.com'
+                    },
+                    {
+                        id: 'julie',
+                        name: 'Julie Jones',
+                        phone: '0321 79283 24',
+                        email: 'julie@localhost.com'
+                    }
+                ],
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

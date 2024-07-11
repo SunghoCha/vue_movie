4e291c30f48af8db940ff111b91454b1d8191b9e
SunghoCha <tjdgh3725@gmail.com>
2024-07-11 11:41:50 +0900

    Monster_Slayer - 전투기록 추가 및 마무리

--- vue_standard procedure/prj-monster-01-starting-setup/app.js
+++ vue_standard procedure/prj-monster-01-starting-setup/app.js
@@ -10,6 +10,7 @@ const app = Vue.createApp({
             monsterHealth: 100,
             attackCount: 0,
             winner: null,
+            logMessages: [],
         }
     },
     computed: {
@@ -31,7 +32,7 @@ const app = Vue.createApp({
             } else {
                 return true;
             }
-        }
+        },
     },
     watch: {
         playerHealth(value) {
@@ -52,15 +53,21 @@ const app = Vue.createApp({
     methods: {
         attackMonster() {
             this.attackCount++;
-            this.monsterHealth -= getRandomValue(5, 12);
+            const attackValue = getRandomValue(5, 12);
+            this.monsterHealth -= attackValue;
+            this.addLogMessage('player', 'attack', attackValue);
             this.attackPlayer();
         },
         attackPlayer() {
-            this.playerHealth -= getRandomValue(8, 15);
+            const attackValue = getRandomValue(8, 15);
+            this.playerHealth -= attackValue;
+            this.addLogMessage('monster', 'attack', attackValue);
         },
         specialAttackMonster() {
-            this.attackCount++;           
-            this.monsterHealth -= getRandomValue(10, 25);
+            this.attackCount++; 
+            const attackValue = getRandomValue(10, 25);     
+            this.monsterHealth -= attackValue;
+            this.addLogMessage('player', 'special-attack', attackValue);
             this.attackPlayer();
         },
         healPlayer() {
@@ -71,17 +78,26 @@ const app = Vue.createApp({
             } else {
                 this.playerHealth += healValue;
             }
+            this.addLogMessage('player', 'heal', healValue);
             this.attackPlayer();
         },
         restart() {
             this.playerHealth = 100,
             this.monsterHealth = 100,
             this.attackCount = 0,
-            this.winner = null;
+            this.winner = null,
+            this.logMessages = [];
         },
         surrender() {
             this.winner = 'monster';
         },
+        addLogMessage(who, what, value) {
+            this.logMessages.unshift({
+                actionBy: who,
+                actionType: what,
+                actionValue: value
+            }); // push는 배열의 마지막에 데이터를 추가하지만 unshift는 배열의 맨 앞에 데이터 추가
+        }
     },
 }) ;
 

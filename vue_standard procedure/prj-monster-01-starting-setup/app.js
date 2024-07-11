81787f709517d11dd4602e2165a1c238461def31
SunghoCha <tjdgh3725@gmail.com>
2024-07-11 09:19:20 +0900

    Monster_Slayer -  회복 기능 추가

--- vue_standard procedure/prj-monster-01-starting-setup/app.js
+++ vue_standard procedure/prj-monster-01-starting-setup/app.js
@@ -40,7 +40,16 @@ const app = Vue.createApp({
             this.attackCount++;           
             this.monsterHealth -= getRandomValue(10, 25);
             this.attackPlayer();
-           
+        },
+        healPlayer() {
+            this.currentRound++;
+            const healValue = getRandomValue(8, 20);
+            if (this.playerHealth + healValue > 100) {
+                this.playerHealth = 100;
+            } else {
+                this.playerHealth += healValue;
+            }
+            this.attackPlayer();
         }
     },
 }) ;

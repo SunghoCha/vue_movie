41ab022209fbf95860c462a0c8f75bf09fe30fa8
SunghoCha <tjdgh3725@gmail.com>
2024-07-12 09:01:50 +0900

    Component - 부모-자식 통신 기초 케이스

---
+++ vue_standard procedure/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,36 @@
+<template>
+    <ul>
+        <li>
+            <h2>{{ name }}</h2>
+            <button @click="toggleDetails">{{detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+            <ul v-if="detailsAreVisible">
+                <li><strong>Phone:</strong> {{ phoneNumber }} </li>
+                <li><strong>Email:</strong> {{ emailAddress }} </li>
+            </ul>
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        props: [
+            'name',
+            'phoneNumber',
+            'emailAddress'
+        ],
+        data() {
+            return {                
+                detailsAreVisible : false,
+            }
+        },
+        methods: {
+            toggleDetails() {
+                this.detailsAreVisible = !this.detailsAreVisible;                
+            }
+        },
+    }
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Jost&display=swap');
+</style>

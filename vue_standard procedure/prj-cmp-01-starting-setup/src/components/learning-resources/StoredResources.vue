0e05927db9ef854efc10ba2b476b88140118582e
SunghoCha <tjdgh3725@gmail.com>
2024-07-14 21:57:44 +0900

    semi - resource 등록 폼 추가 및 스타일링

--- vue_standard procedure/prj-cmp-01-starting-setup/src/components/learning-resources/StoredResources.vue
+++ vue_standard procedure/prj-cmp-01-starting-setup/src/components/learning-resources/StoredResources.vue
@@ -16,7 +16,7 @@ import LearningResource from './LearningResource.vue';
         components: {
             LearningResource,
         },
-        props: ['resources']
+        inject: ['resources']
     }
 </script>
 
---
+++ vue_standard procedure/prj-cmp-01-starting-setup/src/components/learning-resources/TheResources.vue
@@ -0,0 +1,60 @@
+<template>
+	<base-card>
+		<base-button @click="setSelectedTab('stored-resources')" :mode="storeResourcesButtonMode">Stored Resources</base-button>
+		<base-button @click="setSelectedTab('add-resources')" :mode="addResourcesButtonMode">Add Resources</base-button>
+	</base-card>
+	<component :is="selectedTab"></component>
+</template>
+
+<script>
+import AddResources from './AddResources.vue';
+import StoredResources from './StoredResources.vue';
+	export default {
+		components: {
+			AddResources,
+			StoredResources,
+		},
+		data() {
+			return {
+				selectedTab: 'stored-resources',
+				storedResources: [
+                    { 
+                        id: 'official-guide',
+                        title: 'Official Guide',
+                        description: 'The official Vue.js documentation',
+                        link: 'https://vuejs.org'
+                    },
+                    {
+                        id: 'google',
+                        title: 'Google',
+                        description: 'Learn to google...',
+                        link: 'https://google.org'
+                    }
+                ],
+			};
+		},
+		provide() {
+			return {
+				resources: this.storedResources	
+			};
+		},
+		computed: {
+			storeResourcesButtonMode() {
+				return this.selectedTab === 'stored-resources' ? null : 'flat';
+			},
+			addResourcesButtonMode() {
+				return this.selectedTab === 'add-resources' ? null : 'flat';
+			}
+		},
+		methods: {
+			setSelectedTab(tab) {
+				this.selectedTab = tab;
+			}
+		}
+		
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

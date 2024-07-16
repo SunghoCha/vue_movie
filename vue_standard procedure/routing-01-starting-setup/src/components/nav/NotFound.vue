d3e7f34dc824d9e2bd0f3f116eaaf46f485029d6
SunghoCha <tjdgh3725@gmail.com>
2024-07-17 07:37:44 +0900

    update commit

---
+++ vue_standard procedure/routing-01-starting-setup/src/components/nav/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+<h2>Page not found! 
+	<router-link to="/teams">이동하기</router-link>
+</h2>>
+</template>
+
+<script>
+	export default {
+		
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

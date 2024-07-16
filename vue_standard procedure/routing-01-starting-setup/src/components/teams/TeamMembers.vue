d00c95e8ba51af51fbc3916d11de08e3a76d0df3
SunghoCha <tjdgh3725@gmail.com>
2024-07-16 23:54:40 +0900

    router - 라우팅에서 props 설정해서 데이터 전달하기

--- vue_standard procedure/routing-01-starting-setup/src/components/teams/TeamMembers.vue
+++ vue_standard procedure/routing-01-starting-setup/src/components/teams/TeamMembers.vue
@@ -17,6 +17,7 @@ import UserItem from '../users/UserItem.vue';
 
 export default {
   inject: ['users', 'teams'],
+  props: ['teamId'],
   components: {
     UserItem
   },
@@ -27,19 +28,41 @@ export default {
     };
   },
   methods: {
-	loadTeamMembers(route) {
-		const teamId = route.params.teamId; // $route는 로드한 라우트에 대한 최신정보를 담고 있음. url이 바뀌면 업데이트되므로 언제나 최신 매개변수를 포함
-		const selectedTeam = this.teams.find(team => team.id === teamId);
-		const members = selectedTeam.members;
-
-		for (const member of members) {
-			const selectedUser = this.users.find(user => user.id === member)
-			this.members.push(selectedUser);
-		}
-		this.teamName = selectedTeam.name;
-	}
+    loadTeamMembers(teamId) {
+      const selectedTeam = this.teams.find(team => team.id === teamId);
+      const members = selectedTeam.members;
+      for (const member of members) {
+        const selectedUser = this.users.find(user => user.id === member)
+        this.members.push(selectedUser);
+      }
+      this.teamName = selectedTeam.name;
+    }
   },
-//   created() { // vue는 캐시 기능을 사용함. -> url이 바뀐다고 컴포는트가 파기되고 새로 created 되지 않기 때문에 url만 바뀌고 화면에는 변화가 없게됨.
+  created() {
+    this.loadTeamMembers(this.teamId);
+  },
+  watch: {
+    teamId(newId) {
+      this.loadTeamMembers(newId);
+    }
+  }
+
+  // ====== 라우팅을 이용해 이동 시 $route에 담긴 정보 바탕으로 로직 구현 ====== (라우팅(url) 변경되더라도 정보 업데이트 안되는 문제)
+	// loadTeamMembers(route) {  //* 라우터는 기본 설정상 로드된 컴포넌트에 프로퍼티(props)를 추가하지 않으므로 props를 추가로 설정해줘야함(route 설정하는 곳에서) 
+	// 	const teamId = route.params.teamId; // $route는 로드한 라우트에 대한 최신정보를 담고 있음. url이 바뀌면 업데이트되므로 언제나 최신 매개변수를 포함
+	// 	const selectedTeam = this.teams.find(team => team.id === teamId);
+	// 	const members = selectedTeam.members;
+
+	// 	for (const member of members) {
+	// 		const selectedUser = this.users.find(user => user.id === member)
+	// 		this.members.push(selectedUser);
+	// 	}
+	// 	this.teamName = selectedTeam.name;
+	// }
+
+
+// ====== $route를 활용한 방법 ======
+//   created() { // vue는 캐시 기능을 사용함. -> url이 바뀐다고 컴포넌트가 파기되고 새로 created 되지 않기 때문에 url만 바뀌고 화면에는 변화가 없게됨.
 // 	const teamId = this.$route.params.teamId; // $route는 로드한 라우트에 대한 최신정보를 담고 있음. url이 바뀌면 업데이트되므로 언제나 최신 매개변수를 포함
 // 	const selectedTeam = this.teams.find(team => team.id === teamId);
 // 	const members = selectedTeam.members;
@@ -50,14 +73,16 @@ export default {
 // 	}
 // 	this.teamName = selectedTeam.name;
 //   },
-	create() {
-    this.loadTeamMembers(this.$route);
-	},
-  watch: {
-    $route(newRoute) { // 변경된 $route값인 newRoute가 인수로 전달됨
-      this.loadTeamMembers(newRoute)
-		}
-  }
+// ====== 라우팅이 변경되면 기존 정보도 업데이트 하도록 watcher 추가 ====== (다른 곳에서 컴포넌트 재사용이 힘들어짐. 변수전달 방식(props) 사용불가)
+	// create() {
+  //   this.loadTeamMembers(this.$route);
+	// },
+  // watch: {
+  //   $route(newRoute) { // 변경된 $route값인 newRoute가 인수로 전달됨
+  //     this.loadTeamMembers(newRoute)
+	// 	}
+  // }
+
 };
 </script>
 

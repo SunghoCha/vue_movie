6b6787a787cc80d6c1c82fcaa6597e194f877adc
SunghoCha <tjdgh3725@gmail.com>
2024-07-21 08:06:20 +0900

    코치 임시데이터 추가 및 목록 작업

--- vue_standard procedure/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ vue_standard procedure/main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -7,15 +7,47 @@
 			<button>Refresh</button>
 			<router-link to="/register">Register as Coach</router-link>
 		</div>
+		<ul v-if="hasCoaches">
+			<coach-item v-for="coach in filteredCoaches" 
+				:key="coach.id" 
+				:id="coach.id" 
+				:first-name="coach.firstName"
+				:last-name="coach.lastName"
+				:rate="coach.hourlyRate"
+				:areas="coach.areas"
+			>
+			</coach-item>
+		</ul>
+		<h3 v-else>No Coaches found.</h3>
 	</section>
 </template>
 
 <script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
 	export default {
-		
+		components: {
+			CoachItem
+		},
+		computed: {
+			filteredCoaches() {
+				return this.$store.getters['coaches/coaches'] // [namespace/getterName]
+			},
+			hasCoaches() {
+				return this.$store.getters['coaches/hasCoaches']
+			}
+		}
 	}
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
 
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
 </style>

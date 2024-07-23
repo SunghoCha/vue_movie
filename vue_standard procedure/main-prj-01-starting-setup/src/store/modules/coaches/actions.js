0f63be9b3ede6573c7d025dcd6031cb240011dfa
SunghoCha <tjdgh3725@gmail.com>
2024-07-23 22:11:23 +0900

    main - POST 요청으로 코치 데이터 저장

--- vue_standard procedure/main-prj-01-starting-setup/src/store/modules/coaches/actions.js
+++ vue_standard procedure/main-prj-01-starting-setup/src/store/modules/coaches/actions.js
@@ -1,14 +1,30 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
       hourlyRate : data.rate,
       areas: data.areas
     };
+  
+    const response = await fetch(`http://localhost:80/coaches/${userId}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'  // 이 부분을 추가합니다.
+      },
+      body: JSON.stringify(coachData)
+    })
 
-    context.commit('registerCoach', coachData);
+    //const responseData = await response.json();
+
+    if (!response.ok) {
+      // 에러 핸들링...
+    }
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId
+    });
   }
-}
+};

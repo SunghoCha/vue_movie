86171d51802c8421f7d3abe9a75f9089428e5abc
SunghoCha <tjdgh3725@gmail.com>
2024-07-24 12:26:36 +0900

    main - backend - 코치 전d(demo)²´ 리스트 가져는 로직 구현

--- vue_restAPI_prac/src/main/java/com/sh/vue_restap_prac/service/CoachService.java
+++ vue_restAPI_prac/src/main/java/com/sh/vue_restap_prac/service/CoachService.java
@@ -1,11 +1,16 @@
 package com.sh.vue_restap_prac.service;
 
 import com.sh.vue_restap_prac.controller.request.CoachCreate;
+import com.sh.vue_restap_prac.controller.response.CoachResponse;
 import com.sh.vue_restap_prac.domain.Coach;
 import com.sh.vue_restap_prac.repository.CoachRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @RequiredArgsConstructor
 @Service
@@ -16,7 +21,12 @@ public class CoachService {
 
     public void register(String userId, CoachCreate coachCreate) {
         Coach coach = coachCreate.toEntity();
-
         coachRepository.save(coach);
+    }
+
+    public List<CoachResponse> getList() {
+        return coachRepository.findAll().stream()
+                .map(CoachResponse::of)
+                .collect(toList());
     }
 }
